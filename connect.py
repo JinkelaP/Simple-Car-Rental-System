448dbfc7ac71166baeb7284dbe6cedf785560095
Haochen Sam Zhu <73946565+JinkelaP@users.noreply.github.com>
2023-07-26 02:04:39 +1200

    master commit
    A LOT

---
+++ connect.py
@@ -0,0 +1,13 @@
+# These details are available on the first MySQL Workbench screen
+# Usually called 'Local Instance'
+dbuser = "root" #PUT YOUR MySQL username here - usually root
+dbpass = "JinkelaP2333" #PUT YOUR PASSWORD HERE
+dbhost = "localhost" 
+dbport = "3306"
+dbname = "carRental"
+
+# dbuser = "HaochenZhu2023" #PUT YOUR MySQL username here - usually root
+# dbpass = "JinkelaP2333" #PUT YOUR PASSWORD HERE
+# dbhost = "HaochenZhu2023.mysql.pythonanywhere-services.com"
+# dbport = "3306"
+# dbname = "HaochenZhu2023$default"
